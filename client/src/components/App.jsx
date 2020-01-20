c5985a4e9d6671c4319d8085f43b8a888007fa2d
cadecameron <cade.cameron@gmail.com>
2020-01-20 14:05:04 -0800

    COMPLETED Level 0

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import MovieListView from './MovieListView.jsx';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [
+        { title: 'Mean Girls' },
+        { title: 'Hackers' },
+        { title: 'The Grey' },
+        { title: 'Sunshine' },
+        { title: 'Ex Machina' },
+      ]
+    }
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>
+            MovieList
+          </h1>
+        </div>
+        <div id="list">
+          <MovieListView movies={this.state.movies}/>
+        </div>
+      </div>
+    );
+  }
+
+};
 
 export default App;
--- client/src/components/MovieListView.jsx
+++ client/src/components/MovieListView.jsx
@@ -1,5 +1,27 @@
+import React from 'react';
+
 class MovieListView extends React.Component {
   constructor(props) {
-    
+    super(props);
+
+    // state (if any)
+  }
+
+  // this bindings (if any)
+
+  // functions
+
+  render() {
+    let list = this.props.movies.map((movie) => 
+    <ul key={movie.title} className="movie">
+      <li>{movie.title}</li>
+    </ul>
+    );
+
+    return (
+      <div className="movie-list">{list}</div>
+    )
   }
 }
+
+export default MovieListView;
